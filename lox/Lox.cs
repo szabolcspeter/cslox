29fcbc919d8491d6f92c01b6f41e12e0da1bb23f
Szabolcs József Péter <Szabolcs-Jozsef.Peter@t-systems.com>
2023-08-10 14:28:06 +0200

    Interpreter skeleton

---
+++ lox/Lox.cs
@@ -0,0 +1,83 @@
+﻿using System.Text;
+
+namespace lox
+{
+    class Lox
+    {
+        static bool hadError = false;
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                if (args.Length > 1)
+                {
+                    Console.WriteLine("Usage: lox [script]");
+                    Environment.Exit(64);
+                }
+                else if (args.Length == 1)
+                {
+                    RunFile(args[0]);
+                }
+                else
+                {
+                    RunPrompt();
+                }
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception occurred: {ex.Message}\nStackTrace: {ex.StackTrace}");
+            }
+        }
+
+        public static void Error(int line, string message)
+        {
+            Report(line, "", message);
+        }
+
+        #region private methods
+
+        private static void RunFile(string path)
+        {
+            if (!File.Exists(path))
+                throw new FileNotFoundException($"File: \"{path}\" not found");
+
+            byte[] bytes = File.ReadAllBytes(Path.GetFullPath(path));
+            Run(Encoding.Default.GetString(bytes, 0, bytes.Length));
+            if (hadError) Environment.Exit(65);
+        }
+
+        private static void RunPrompt()
+        {
+            while (true)
+            {
+                System.Console.Write("> ");
+                string line = Console.ReadLine();
+                if (string.IsNullOrWhiteSpace(line))
+                {
+                    Console.WriteLine("No more input received.");
+                    break;
+                }
+                Run(line);
+                hadError = false;
+            }
+        }
+
+        private static void Run(string source)
+        {
+            // throw new NotImplementedException();
+        }
+
+        private static void Report(int line, string where, string message)
+        {
+            Console.WriteLine("[line " + line + "] Error" + where + ": " + message);
+            hadError = true;
+        }
+
+        #endregion
+    }
+}
